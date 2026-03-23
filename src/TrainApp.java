0b467860765eae5a624b39507648f9c0fe6f7e84
Ashrafkkhan <mohdashrafkkhan@gmail.com>
2026-03-23 11:14:52 +0530

    Update TrainApp.java

--- src/TrainApp.java
+++ src/TrainApp.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
 
 public class TrainApp {
+  public static void main(String[] args) {
+// Display welcome banner
+System.out.println(" ");
+System.out.println(" Train Consist Management App");
+System.out.println(" \n");
+// Create a dynamic list to store train bogies
+List<String> trainConsist = new ArrayList();
+// Display initial consist information
+System.out.println("Train initialized successfully...");
 }

933ca93b43ad9b0930efdef5fe025eb5f1565e65
Ashraf Khan <mohdashrafkkhan@gmail.com>
2026-04-06 21:39:10 +0530

    UC7: Add Bogie class and sort by capacity

---
+++ src/TrainConsistApp.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Bogie {
+    String name;
+    int capacity;
+
+    Bogie(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    public String toString() {
+        return name + " - " + capacity;
+    }
+}
+
+public class TrainConsistApp {
+
+    public static void main(String[] args) {
+
+        List<Bogie> bogies = new ArrayList<>();
+
+        bogies.add(new Bogie("Sleeper", 72));
+        bogies.add(new Bogie("AC Chair", 60));
+        bogies.add(new Bogie("First Class", 30));
+
+        bogies.sort(Comparator.comparingInt(b -> b.capacity));
+
+        for (Bogie b : bogies) {
+            System.out.println(b);
+        }
+    }
+}

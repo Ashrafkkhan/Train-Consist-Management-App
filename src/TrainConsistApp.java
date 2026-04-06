bbeaca4b9276dc1b0d3b30ee8974e55250e20b81
Ashraf Khan <mohdashrafkkhan@gmail.com>
2026-04-06 21:59:09 +0530

    UC14: Add InvalidCapacityException for bogie validation

--- src/TrainConsistApp.java
+++ src/TrainConsistApp.java
@@ -1,13 +1,28 @@
 import java.util.*;
-import java.util.stream.Collectors;
+
+class InvalidCapacityException extends Exception {
+    public InvalidCapacityException(String message) {
+        super(message);
+    }
+}
 
 class Bogie {
     int id;
     int capacity;
+    String type;
 
-    public Bogie(int id, int capacity) {
+    public Bogie(int id, int capacity, String type) throws InvalidCapacityException {
+        if (capacity <= 0) {
+            throw new InvalidCapacityException("Capacity must be greater than 0!");
+        }
         this.id = id;
         this.capacity = capacity;
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Bogie ID: " + id + ", Capacity: " + capacity + ", Type: " + type;
     }
 }
 
@@ -16,32 +31,18 @@ public class TrainConsistApp {
     public static void main(String[] args) {
 
         List<Bogie> bogies = new ArrayList<>();
-        for (int i = 1; i <= 100000; i++) {
-            bogies.add(new Bogie(i, (i % 100) + 1));
+
+        try {
+
+            bogies.add(new Bogie(1, 60, "Passenger"));
+
+            bogies.add(new Bogie(2, 0, "Passenger"));
+
+        } catch (InvalidCapacityException e) {
+            System.out.println("Error: " + e.getMessage());
         }
 
-        long startLoop = System.nanoTime();
-
-        List<Bogie> loopResult = new ArrayList<>();
-        for (Bogie b : bogies) {
-            if (b.capacity > 60) {
-                loopResult.add(b);
-            }
-        }
-
-        long endLoop = System.nanoTime();
-        long loopTime = endLoop - startLoop;
-
-        long startStream = System.nanoTime();
-
-        List<Bogie> streamResult = bogies.stream()
-                .filter(b -> b.capacity > 60)
-                .collect(Collectors.toList());
-
-        long endStream = System.nanoTime();
-        long streamTime = endStream - startStream;
-
-        System.out.println("Loop Time (ns): " + loopTime);
-        System.out.println("Stream Time (ns): " + streamTime);
+        System.out.println("\nFinal Bogie List:");
+        bogies.forEach(System.out::println);
     }
 }

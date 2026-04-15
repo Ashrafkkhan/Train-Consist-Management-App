fcc659a68464b4c9b191710fdb56e8ed4d6e3766
Ashraf Khan <mohdashrafkkhan@gmail.com>
2026-04-15 21:20:46 +0530

    UC15: handle runtime cargo safely using try-catch-finally

--- src/TrainConsistApp.java
+++ src/TrainConsistApp.java
@@ -6,10 +6,17 @@ class InvalidCapacityException extends Exception {
     }
 }
 
+class UnsafeCargoException extends Exception {
+    public UnsafeCargoException(String message) {
+        super(message);
+    }
+}
+
 class Bogie {
     int id;
     int capacity;
     String type;
+    String cargo;
 
     public Bogie(int id, int capacity, String type) throws InvalidCapacityException {
         if (capacity <= 0) {
@@ -20,9 +27,18 @@ class Bogie {
         this.type = type;
     }
 
+    public void assignCargo(String cargoType) throws UnsafeCargoException {
+        if (type.equalsIgnoreCase("Passenger") && cargoType.equalsIgnoreCase("Petroleum")) {
+            throw new UnsafeCargoException("Cannot assign petroleum to passenger bogie!");
+        }
+        this.cargo = cargoType;
+        System.out.println("Cargo assigned successfully to Bogie " + id);
+    }
+
     @Override
     public String toString() {
-        return "Bogie ID: " + id + ", Capacity: " + capacity + ", Type: " + type;
+        return "Bogie ID: " + id + ", Capacity: " + capacity +
+               ", Type: " + type + ", Cargo: " + (cargo == null ? "None" : cargo);
     }
 }
 
@@ -33,13 +49,22 @@ public class TrainConsistApp {
         List<Bogie> bogies = new ArrayList<>();
 
         try {
-
             bogies.add(new Bogie(1, 60, "Passenger"));
-
-            bogies.add(new Bogie(2, 0, "Passenger"));
-
+            bogies.add(new Bogie(2, 80, "Cargo"));
         } catch (InvalidCapacityException e) {
-            System.out.println("Error: " + e.getMessage());
+            System.out.println("Creation Error: " + e.getMessage());
+        }
+
+        System.out.println("\n--- Assigning Cargo ---");
+
+        for (Bogie b : bogies) {
+            try {
+                b.assignCargo("Petroleum");
+            } catch (UnsafeCargoException e) {
+                System.out.println("Runtime Error: " + e.getMessage());
+            } finally {
+                System.out.println("Checked Bogie ID: " + b.id);
+            }
         }
 
         System.out.println("\nFinal Bogie List:");
